a16b6b461b7c5070f69ad89545b227f7e2e1e701
Dmitry Kovalev <kdn.kovalev@gmail.com>
2019-03-22 01:02:36 -0600

    ensure ThreadLocal ScopeManager is used in tests

--- scala/src/test/scala/com/gihub/fehu/opentracing/Spec.scala
+++ scala/src/test/scala/com/gihub/fehu/opentracing/Spec.scala
@@ -3,13 +3,13 @@ package com.gihub.fehu.opentracing
 import scala.collection.convert.DecorateAsScala
 
 import io.opentracing.mock.{ MockSpan, MockTracer }
-import io.opentracing.util.GlobalTracer
+import io.opentracing.util.{ GlobalTracer, ThreadLocalScopeManager }
 import org.scalatest.{ BeforeAndAfter, Matchers, Suite }
 
 trait Spec extends Matchers with BeforeAndAfter with DecorateAsScala {
   this: Suite =>
 
-  val mockTracer = new MockTracer()
+  val mockTracer = new MockTracer(new ThreadLocalScopeManager)
 
   before {
     mockTracer.reset()
