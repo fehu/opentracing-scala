8e1c751f680ec9890eb41d5a1519c06636b43b8e
Dmitry Kovalev <kdn.kovalev@gmail.com>
2020-07-20 03:31:29 -0500

    Fix `AskTracing`.

--- akka/src/main/scala/com/github/fehu/opentracing/v2/akka/AskTracing.scala
+++ akka/src/main/scala/com/github/fehu/opentracing/v2/akka/AskTracing.scala
@@ -14,17 +14,16 @@ import com.github.fehu.opentracing.v2.syntax._
 object AskTracing {
   class Ops[F[_]: Async: ContextShift: Traced](ref: ActorRef, message: Any, sender: ActorRef)
                                               (implicit timeout: Timeout) {
-    def traced: F[Any] = trace0(locally)
+    def traced: F[Any] = trace0
 
-    def trace(op: String, tags: Traced.Tag*): F[Any] = trace0(_.trace(op, tags: _*))
+    def trace(op: String, tags: Traced.Tag*): F[Any] = trace0.trace(op, tags: _*)
 
-    private def trace0[R](continue: F[Any] => F[R]): F[R] =
+    private def trace0: F[Any] =
       for {
         ctx <- Traced.currentSpan.context
-        res = Async.fromFuture(
+        res <- Async.fromFuture(
                 Async[F].delay{ pattern.ask(ref, TracedMessage(message, ctx), sender) }
               )
-        r <- continue(res)
-      } yield r
+      } yield res
   }
 }
