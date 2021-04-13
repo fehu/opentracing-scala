bed478ecbf6d7910ee95038906b9f3d8b3ce049c
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-04-13 00:51:09 -0500

    Refactor `fs2` syntax extensions.

--- fs2/src/main/scala/com/github/fehu/opentracing/fs2/package.scala
+++ fs2/src/main/scala/com/github/fehu/opentracing/fs2/package.scala
@@ -12,32 +12,52 @@ import com.github.fehu.opentracing.Traced.ActiveSpan
 package object fs2 {
 
   final implicit class TracedFs2StreamOps[F[_]: Bracket[*[_], Throwable], A](stream: Stream[F, A])(implicit t: Traced[F]) {
-    def trace(operation: String, tags: Traced.Tag*): Stream[F, A] =
-      common(_.spanResource(operation, tags: _*))
+    def traceLifetime(operation: String, tags: Traced.Tag*): Stream[F, A] =
+      for {
+        (span, finish) <- Stream eval t.spanResource(operation, tags: _*).allocated
+        a <- stream.translate(λ[F ~> F](t.recoverCurrentSpan(span) *> _))
+          .onFinalize(finish)
+      } yield a
 
-    def inject(context: SpanContext)(operation: String, tags: Traced.Tag*): Stream[F, A] =
-      common(_.injectContext(context).spanResource(operation, tags: _*))
+    def traceUsage(operation: String, tags: Traced.Tag*): Stream[F, A] =
+      tracingElems(_ => t.spanResource(operation, tags: _*))
 
-    def inject(context: Option[SpanContext])(operation: String, tags: Traced.Tag*): Stream[F, A] =
-      context.map(inject(_)(operation, tags: _*)).getOrElse(stream)
+    def traceUsage(trace: A => Traced.Operation.Builder): Stream[F, A] =
+      tracingElems { a => t.spanResource(trace(a)) }
 
-    def injectFrom[C](format: Format[C])(carrier: C)(operation: String, tags: Traced.Tag*): Stream[F, A] =
-      common(_.injectContextFrom(format)(carrier).spanResource(operation, tags: _*))
+    def traceUsageInjecting(context: A => SpanContext, trace: A => Traced.Operation.Builder): Stream[F, A] =
+      tracingElems { a => t.injectContext(context(a)).spanResource(trace(a)) }
 
-    def injectFromOpt[C](format: Format[C])(carrier: Option[C])(operation: String, tags: Traced.Tag*): Stream[F, A] =
-      carrier.map(injectFrom(format)(_)(operation, tags: _*)).getOrElse(stream)
+    def traceUsageInjectingOpt(context: A => Option[SpanContext], trace: A => Traced.Operation.Builder, traceEmpty: Boolean = true): Stream[F, A] =
+      tracingElems { a =>
+        context(a).fold(
+          if (traceEmpty) t.spanResource(trace(a)) else Resource.pure(ActiveSpan.empty)
+        )(
+          t.injectContext(_).spanResource(trace(a))
+        )
+      }
+
+    def traceUsageInjectingFrom[C](format: Format[C])(carrier: A => C, trace: A => Traced.Operation.Builder): Stream[F, A] =
+      tracingElems { a =>
+        t.injectContextFrom(format)(carrier(a)).spanResource(trace(a))
+      }
+
+    def traceUsageInjectingFromOpt[C](format: Format[C])(carrier: A => Option[C], trace: A => Traced.Operation.Builder, traceEmpty: Boolean = true): Stream[F, A] =
+      tracingElems { a =>
+        carrier(a).fold(
+          if (traceEmpty) t.spanResource(trace(a)) else Resource.pure(ActiveSpan.empty)
+        )(
+          t.injectContextFrom(format)(_).spanResource(trace(a))
+        )
+      }
 
     def tracedLog(f: A => Seq[(String, Any)]): Stream[F, A] =
       stream.evalTap(a => t.currentSpan.log(f(a): _*))
 
     def tracedElemLog: Stream[F, A] = stream.evalTap(t.currentSpan log _.toString)
 
-    private def common(f: Traced[F] => Resource[F, ActiveSpan]) =
-      for {
-        (span, finish) <- Stream eval f(t).allocated
-        a <- stream.translate(λ[F ~> F](t.recoverCurrentSpan(span) *> _))
-                   .onFinalize(finish)
-      } yield a
+    private def tracingElems(f: A => Resource[F, Traced.ActiveSpan]): Stream[F, A] =
+      stream.flatMap(a => Stream.resource(f(a)).as(a))
   }
 
 }
