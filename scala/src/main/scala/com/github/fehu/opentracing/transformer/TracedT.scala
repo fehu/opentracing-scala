f66f6a0a3420a08290ffe2c00268cde8049f7e3d
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-08-05 15:30:09 -0500

    Define `map`, `flatMap`, `flatMapF` and `mapK` at `TracedT`.

--- scala/src/main/scala/com/github/fehu/opentracing/transformer/TracedT.scala
+++ scala/src/main/scala/com/github/fehu/opentracing/transformer/TracedT.scala
@@ -1,6 +1,6 @@
 package com.github.fehu.opentracing.transformer
 
-import cats.~>
+import cats.{ FlatMap, Functor, ~> }
 import cats.data.StateT
 import cats.effect.{ Effect, IO, LiftIO }
 
@@ -8,6 +8,12 @@ import com.github.fehu.opentracing.internal.{ State, TracedTFunctions, TracedTFu
 
 final case class TracedT[F[_], A](stateT: StateT[F, State, A]) extends AnyVal {
   def transform[G[_], B](f: StateT[F, State, A] => StateT[G, State, B]): TracedT[G, B] = copy(f(stateT))
+
+  def map[B](f: A => B)(implicit F: Functor[F]): TracedT[F, B] = transform(_.map(f))
+  def flatMap[B](f: A => TracedT[F, B])(implicit F: FlatMap[F]): TracedT[F, B] = transform(_.flatMap(f andThen (_.stateT)))
+  def flatMapF[B](f: A => F[B])(implicit F: FlatMap[F]): TracedT[F, B] = transform(_.flatMapF(f))
+
+  def mapK[G[_]](fk: F ~> G)(implicit F: Functor[F]): TracedT[G, A] = transform(_.mapK(fk))
 }
 
 object TracedT extends TracedTInstances with TracedTFunctions {
