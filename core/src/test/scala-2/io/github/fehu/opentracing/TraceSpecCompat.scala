84debfc45d19380488586e582bf992f6686db012
Dmitry Kovalev <kdn.kovalev@gmail.com>
2022-06-29 17:32:09 -0500

    Delete unused code

--- core/src/test/scala-2/io/github/fehu/opentracing/TraceSpecCompat.scala
+++
@@ -1,7 +0,0 @@
-package io.github.fehu.opentracing
-
-import io.github.fehu.opentracing.Spec.TestedSpan
-
-object TraceSpecCompat {
-  def expectedTraceTimeoutsTestOverride: Seq[TestedSpan] => Seq[TestedSpan] = locally
-}
