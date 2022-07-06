c12dcd79820e33424091913e2c82f5e7154eeda0
Dmitry Kovalev <kdn.kovalev@gmail.com>
2022-07-05 23:03:39 -0500

    Fix IOSpec

--- core/src/test/scala/io/github/fehu/opentracing/IOSpec.scala
+++ core/src/test/scala/io/github/fehu/opentracing/IOSpec.scala
@@ -4,7 +4,7 @@ import cats.effect.unsafe.{ IORuntime, IORuntimeConfig }
 
 trait IOSpec {
   protected implicit lazy val ioRuntime: IORuntime = {
-    val (compute, compDown) = IORuntime.createDefaultComputeThreadPool(null, threads = ioRuntimeComputeThreads)
+    val (compute, compDown) = IORuntime.createDefaultComputeThreadPool(ioRuntime, threads = ioRuntimeComputeThreads)
     val (blocking, blockDown) = IORuntime.createDefaultBlockingExecutionContext()
     val (scheduler, schedDown) = IORuntime.createDefaultScheduler()
 
